4626ca9c9444f95e072444bf4e8c20139367097a
SANUJAYA200 <109306832+SANUJAYA200@users.noreply.github.com>
2022-07-15 00:13:19 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -19,10 +19,10 @@ global.APIKeys = {
 }
 
 //customize settings\\
-global.owner = ['Your Number']
-global.premium = ['Your Number']
-global.ownernomer = 'Your Number'
-global.ownername = '🐦Star Boy Nexus'
+global.owner = ['94776806270']
+global.premium = ['94776806270']
+global.ownernomer = '94776806270'
+global.ownername = '🐦SANUJAYA'
 global.botname = 'Gojo-Satoru'
 global.footer = '©NexusNwInc.'
 global.ig = 'https://github.com/nexusNw'
